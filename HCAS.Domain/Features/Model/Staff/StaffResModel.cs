f46a9f524116f9274eac55c216b675b358b7c46f
sanneiphyo <sanneiphyo@gmail.com>
2025-09-13 23:17:08 +0630

    feat : Assign Setup

---
+++ HCAS.Domain/Features/Model/Staff/StaffResModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCAS.Domain.Features.Model.Staff
+{
+    internal class StaffResModel
+    {
+    }
+}
