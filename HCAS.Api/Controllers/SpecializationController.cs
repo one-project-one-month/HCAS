e2828985f2d98d90a94d462dd93b156e38618a20
Thet Naing Oo <thetnaing94@gmail.com>
2025-09-14 00:20:02 +0630

    added specializationController

---
+++ HCAS.Api/Controllers/SpecializationController.cs
@@ -0,0 +1,48 @@
+﻿using HCAS.Domain.Features.Specialization;
+using HCAS.Domain.Features.Model.Specialization;
+using HCAS.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HCAS.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SpecializationController : ControllerBase
+    {
+        private readonly SpecializationSerivce _specializationSerivce;
+
+        public SpecializationController(SpecializationSerivce specializationSerivce)
+        {
+            _specializationSerivce = specializationSerivce;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSpecializationList()
+        {
+            var specializationList = await _specializationSerivce.GetAllSpecializationList();
+            return Ok(specializationList.Data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterSpecialization(SpecializationReqModel dto)
+        {
+            var registerSpecialization = await _specializationSerivce.RegisterSpecializations(dto);
+            return Ok(registerSpecialization.Data);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialization(SpecializationReqModel dto)
+        {
+            var updateSpecialization = await _specializationSerivce.UpdateSpecializations(dto);
+            return Ok(updateSpecialization.Data);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialization(int id)
+        {
+            var deleteSpecialization = await _specializationSerivce.DeleteSpecializations(id);
+            return Ok(deleteSpecialization.Data);
+        }
+    }
+}
