074dad95eec860200b4818d38c872bfeb6d4df98
Kyaw Zaw Linn <kyawzawlinn.dev@gmail.com>
2025-09-15 12:21:34 +0800

    feat(domain): add StaffResModel for staff API request

--- HCAS.Domain/Features/Model/Staff/StaffReqModel.cs
+++ HCAS.Domain/Features/Model/Staff/StaffReqModel.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace HCAS.Domain.Features.Model.Staff
 {
-    internal class StaffReqModel
+    public class StaffReqModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string? Phone { get; set; }
+
+        public string? Role { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
     }
 }
